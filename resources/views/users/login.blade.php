ecd324c2828b2fc02656a071adff9ed564fb83af
Sony Muzakki <48709779+sonymuzakki@users.noreply.github.com>
2022-09-08 10:50:29 +0700

    Update login.blade.php
    hp

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <title>TINVES | Login </title>
+  <title>TINVES | LOGIN </title>
   <link rel="icon" href="../assets/img/29.png" width="50 " class="img-rounded-circle">
 
   <!-- General CSS Files -->
